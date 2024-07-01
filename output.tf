704bf99e5ca2e90a2f06c48bf65d5d7392df5f94
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-01 17:11:23 +0200

    fixing syntax

--- output.tf
+++ output.tf
@@ -1,20 +1,20 @@
 # output of server name and id
 
-output "Server1_name: " {
+output "Server1_name" {
   value = aws_instance.test.tags["Name"]
   
 }
-output "Server1_ID: " {
+output "Server1_ID" {
   value = aws_instance.test.id
   
 }
 
-output "Server2_name: " {
+output "Server2_name" {
   value = aws_instance.test1.tags["Name"]
   
 }
 
-output "Server2_ID: " {
+output "Server2_ID" {
   value = aws_instance.test1.id
   
 }
