5d9fa8fcdfe1045847bd6bf59205ff772ed755b0
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-01 21:10:36 +0200

    changing output to single server

--- output.tf
+++ output.tf
@@ -1,25 +1,14 @@
 # output of server name and id
 
 output "Server_1_Name" {
-  value = aws_instance.test.tags["Name"]
+  value = aws_instance.webser.tags["Name"]
 
 }
 output "Server_1_ID" {
-  value = aws_instance.test.id
+  value = aws_instance.webser.id
 
 }
 output "Server_1_IP" {
-  value = aws_instance.test1.public_ip
-
-}
-output "Server_2_Name" {
-  value = aws_instance.test1.tags["Name"]
-
-}
-output "Server_2_ID" {
-  value = aws_instance.test1.id
-}
-output "Server_2_IP" {
-  value = aws_instance.test1.public_ip
+  value = aws_instance.webser.public_ip
 
 }
