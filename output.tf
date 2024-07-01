827b1b5adca924eb683f3501ad8def339d8984e0
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-01 21:25:58 +0200

    adding descrition

--- output.tf
+++ output.tf
@@ -2,13 +2,13 @@
 
 output "Server_1_Name" {
   value = aws_instance.webser.tags["Name"]
-
+  description = "The name of the server"
 }
 output "Server_1_ID" {
   value = aws_instance.webser.id
-
+  description = "The ID of the server"
 }
 output "Server_1_IP" {
   value = aws_instance.webser.public_ip
-
+  description = "The public IP of the server"
 }
