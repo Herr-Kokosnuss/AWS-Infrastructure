5e81a3ca196112537164b51c266ab83f579a7374
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-01 17:07:43 +0200

    adding output file for server name and ID

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 
-
 # Creating ec2 instance
 resource "aws_instance" "test" {
   ami           = "ami-01b799c439fd5516a"
---
+++ output.tf
@@ -0,0 +1,20 @@
+# output of server name and id
+
+output "Server1_name: " {
+  value = aws_instance.test.tags["Name"]
+  
+}
+output "Server1_ID: " {
+  value = aws_instance.test.id
+  
+}
+
+output "Server2_name: " {
+  value = aws_instance.test1.tags["Name"]
+  
+}
+
+output "Server2_ID: " {
+  value = aws_instance.test1.id
+  
+}
