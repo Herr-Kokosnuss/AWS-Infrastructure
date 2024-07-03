9a5edf07fc717525baef7e515bf9f034eb452f3d
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-03 15:31:28 +0200

    changing listener resource

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_launch_configuration" "example" {
   user_data = file("userdata.sh")
 
   # set create_before_destroy to true, Terraform will invert the order in which
-  # it replaces resources, creating the replacement resource first
+  # it replaces resources, creating the replacement resource first to ensure continuity of service (zero downtime)
   lifecycle {
     create_before_destroy = true
   }
@@ -26,17 +26,11 @@ resource "aws_autoscaling_group" "example" {
   vpc_zone_identifier  = data.aws_subnets.default.ids # getting subnets from variable 
 
   target_group_arns = [aws_lb_target_group.asg.arn] # integration between ASG and ALB so the target group knows 
-  # which EC2 instances to route traffic to.
+                                                    # which EC2 instances to route traffic to since autoscaling 
+                                                    #is initializing and terminating instances all the time.
 
 
-  health_check_type = "ELB" #a minimal health check that considers an
-  # Instance unhealthy only if the AWS hypervisor says the VM is completely down or
-  # unreachable. The "ELB" health check is more robust, because it instructs the ASG
-  # to use the target group’s health check to determine whether an Instance is healthy
-  # and to automatically replace Instances if the target group reports them as unhealthy.
-  # That way, Instances will be replaced not only if they are completely down but also if,
-  # for example, they’ve stopped serving requests because they ran out of memory or a
-  # critical process crashed.        
+  health_check_type = "ELB" # health check type is ELB (default is EC2)      
 
   min_size = 2
   max_size = 4
@@ -62,23 +56,7 @@ resource "aws_lb" "example" {
 
 }
 
-# define a listener for the above Application Load Balancer
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-  # By default, return a simple 404 page.
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-# create a target group for your ASG
+# create a target group for ASG
 #target group will health check your Instances by periodically sending an HTTP
 #request to each Instance and will consider the Instance “healthy” only if the Instance
 #returns a response that matches the configured matcher (e.g., you can configure a
@@ -99,16 +77,13 @@ resource "aws_lb_target_group" "asg" {
   }
 }
 
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-  action {
+# define a listener for the above Application Load Balancer
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
+    target_group_arn = aws_lb_target_group.asg.arn   
+  } 
 }
