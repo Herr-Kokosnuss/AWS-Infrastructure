3f6835433330dd3b2317652bafbcccc27c0418c6
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-03 14:20:20 +0200

    created file for security groups

---
+++ sg.tf
@@ -0,0 +1,44 @@
+######### Security Groups ######
+# Creating a security group for the Application Load Balancer
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Creating a security group for the instance
+# resource "aws_security_group" "test" {
+#   name = "terraform-example-instance"
+#   # ingress {
+#   #   from_port   = 8080
+#   #   to_port     = 8080
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = ["0.0.0.0/0"]
+#   # }
+#    ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   # Allow all outbound requests
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+# }
