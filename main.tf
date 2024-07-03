855a27069c713862c072f371cfd4e58a60396257
Ahmed Al-Saeeedi <153365040+The-Anonymous-Frog@users.noreply.github.com>
2024-07-03 22:54:50 +0200

    syntax

--- main.tf
+++ main.tf
@@ -10,8 +10,7 @@ resource "aws_launch_configuration" "example" {
   image_id        = "${var.ami}"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.alb.id]
-
-  user_data = file("userdata.sh")
+  user_data       = file("userdata.sh") 
 
   # set create_before_destroy to true, Terraform will invert the order in which
   # it replaces resources, creating the replacement resource first to ensure continuity of service (zero downtime)
