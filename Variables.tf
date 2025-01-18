19ae74a67f319be953fcdf3d2052664ee659f4da
Ahmed Al-Saeedi <ahmed.alsaeedi7890@gmail.com>
2025-01-18 17:02:25 +0100

    final cleaning

--- Variables.tf
+++ Variables.tf
@@ -46,7 +46,7 @@ variable "public_subnet_cidrs" {
 # variable "ami" {
 #   default     = "ami-01b799c439fd5516a"
 #   description = "value of the ami"
-#   #type = string # we can remove default and add type. this will prompt the user to enter the value of the variable in the terminal. 
+#   #type = string 
 # }
 
 # # look up the subnets within VPC.
