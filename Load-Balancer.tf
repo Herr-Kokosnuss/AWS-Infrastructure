6708a6b8b3951a2641a93a25adf510d0c58f9890
Ahmed Al-Saeedi <ahmed.alsaeedi7890@gmail.com>
2025-01-19 01:46:33 +0100

    added https listener to laod balancer

--- Load-Balancer.tf
+++ Load-Balancer.tf
@@ -1,3 +1,7 @@
+# Data sources for AWS region and account ID
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 #Amazon's Elastic Load Balancer (ELB)
 resource "aws_lb" "Cocoplanner" {
   name               = "Cocoplanner"
@@ -23,12 +27,30 @@ resource "aws_lb_target_group" "asg" {
   }
 }
 
-# define a listener for the above Application Load Balancer
+# HTTP Listener - Redirect to HTTPS
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.Cocoplanner.arn
   port              = 80
   protocol          = "HTTP"
   
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# HTTPS Listener - Forward to target group
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.Cocoplanner.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:certificate/c6475e8c-127f-4d84-8641-e4cfc75e99fa"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
