0c9606f2adabe29de9e040082f688642e9d1b02e
Ahmed-AlSaeedi <ahmed.alsaeedi@neurobazar.com>
2025-03-23 23:18:44 +0100

    change instances

--- ASG.tf
+++ ASG.tf
@@ -43,9 +43,9 @@ resource "aws_autoscaling_group" "Cocoplanner" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size         = 2
-  max_size         = 2
-  desired_capacity = 2
+  min_size         = 1
+  max_size         = 1
+  desired_capacity = 1
 
   tag {
     key                 = "Name"
